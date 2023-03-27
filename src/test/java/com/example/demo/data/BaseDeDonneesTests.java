6d5b1152ee775a29ab54c2945be2003cf3e2a995
Yassine492 <yassinebellagraa2@gmail.com>
2023-03-27 17:32:38 +0200

    Tests BDD v.1.0

--- src/test/java/com/example/demo/data/BaseDeDonneesTests.java
+++ src/test/java/com/example/demo/data/BaseDeDonneesTests.java
@@ -3,6 +3,7 @@ package com.example.demo.data;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.util.Assert;
 
 import java.util.Collections;
 
@@ -16,8 +17,17 @@ public class BaseDeDonneesTests {
 
     @Test
     void uneVoiture(){
-        Voiture v = new Voiture("Fiat", 4000);
+        Voiture v = new Voiture("Fiat", 40000);
+        when(voitureRepository.save(v)).thenReturn(v);
+        voitureRepository.save(v);
+        Voiture voitureRecuperee = (Voiture) voitureRepository.findById(0);
+        Assert.isTrue(v.equals(voitureRecuperee), "La voiture récupérée n'est pas la même que celle créée");
+        voitureRepository.deleteById(voitureRecuperee.getId());
+        voitureRecuperee = voitureRepository.findById(0);
+        Assert.isTrue(v != voitureRecuperee, "La voiture existe toujours dans la BDD");
 
+        // tester les méthodes de l'interface CrudRepository qui permette d'accéder à la base de données: https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
+        // save, find, delete...
     }
 
 }
